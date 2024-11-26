a833866018422e6b825b44a2129de52e100106ed
dulanipali <dulani.2020@gmail.com>
2024-11-26 02:17:16 -0600

    Added firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+//NODE.js / Next.js version...???
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";  // Import Firebase Storage
@@ -22,3 +23,26 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { db, storage };
+
+{/* 
+  JAVASCRIP VERSION
+
+  <script type="module">
+  // Import the functions you need from the SDKs you need
+  import {initializeApp} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBzF9rQIiRhQTCPPTY93Fjvct8aDsJtHIM",
+  authDomain: "movie-booking-system-119eb.firebaseapp.com",
+  projectId: "movie-booking-system-119eb",
+  storageBucket: "movie-booking-system-119eb.firebasestorage.app",
+  messagingSenderId: "582758548972",
+  appId: "1:582758548972:web:2088b20c9cb3f1a0b42cc9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script> */}
